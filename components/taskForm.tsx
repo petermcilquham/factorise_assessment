c1ed980124e054d6e985dcee52366c0a56d284f4
petermcilquham <peter.lm.schmidt@gmail.com>
2025-08-12 15:10:01 +0200

    small changes to taskForm and removing of unnecessary files

--- components/taskForm.tsx
+++ components/taskForm.tsx
@@ -14,8 +14,9 @@ export function TaskForm() {
   const [task, setTask] = useState<Task>({ title: '', description: '' } as Task);
   const [taskList, setTaskList] = useState<Task[]>([]);
   const [errors, setErrors] = useState<Errors>({} as Errors);
-  const isFormValidated = useRef(false);
+  const isFormValidated = useRef<any>(false);
   isFormValidated.current = Object.keys(errors).length === 0;
+  const inputRef = useRef<any>(null);
 
   // functions
   function handleSubmit() {
@@ -28,12 +29,16 @@ export function TaskForm() {
     if (task.title.length < 3) errorMessage.title = 'Title must be at least 3 characters long';
     setErrors(errorMessage);
   }, [task]);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [task.title]);
 
   return (
     <View style={styles.taskForm}>
       <KeyboardAvoidingView behavior='padding'>
         <View style={styles.form}>
           <TextInput
+            ref={inputRef}
             style={[styles.input, task.title.length !== 0 && !isFormValidated.current ? styles.redBorder : null]}
             value={task.title}
             onChangeText={(text) => setTask({ ...task, title: text })}
@@ -41,7 +46,7 @@ export function TaskForm() {
           />
           <TextInput multiline={true} numberOfLines={4} style={styles.input} value={task.description} onChangeText={(text) => setTask({ ...task, description: text })} placeholder='Task description' />
           {task.title.length !== 0 && !isFormValidated.current ? <Text style={styles.errorText}>{errors.title}</Text> : null}
-          {task.description!.length !== 0 && !isFormValidated.current ? <Text style={styles.errorText}>{errors.description!}</Text> : null}
+          {task.description!.length !== 0 && !isFormValidated.current && errors.description! ? <Text style={styles.errorText}>{errors.description}</Text> : null}
           <Pressable onPress={handleSubmit} style={[styles.submitButton, !isFormValidated.current ? styles.disabled : null]} disabled={!isFormValidated.current}>
             <Text>Submit Task</Text>
           </Pressable>
@@ -94,18 +99,17 @@ const styles = StyleSheet.create({
   },
   input: {
     width: 200,
-    backgroundColor: '#E1E1E1',
+    backgroundColor: '#fdc19f44',
     borderRadius: 20,
     paddingLeft: 10,
   },
   submitButton: {
-    marginTop: 10,
     borderColor: 'black',
     borderStyle: 'solid',
     borderWidth: 1,
     width: 120,
     height: 40,
-    backgroundColor: '#E1E1E1',
+    backgroundColor: '#fdc19f44',
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
@@ -115,12 +119,12 @@ const styles = StyleSheet.create({
     borderColor: 'grey',
   },
   redBorder: {
-    borderColor: 'tomato',
+    borderColor: 'red',
     borderStyle: 'solid',
     borderWidth: 1,
   },
   errorText: {
-    color: 'tomato',
+    color: 'red',
   },
   listContainer: {
     flexGrow: 0,
